0f07fdc88d464500b20ad3764d844f9df11478ac
Sara <sara@thoughtstem.com>
2019-11-15 16:15:30 -0800

    update info

--- info.rkt
+++ info.rkt
@@ -1,8 +1,6 @@
 #lang info
-;(define collection "pzuh-assets")
-(define deps '("define-assets-from"))
-;(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+
+(define deps '("https://github.com/thoughtstem/define-assets-from.git"))
+
 (define scribblings '(("scribblings/pzuh-assets.scrbl" ())))
-;(define pkg-desc "Description Here")
-;(define version "0.0")
-;(define pkg-authors '(saralucchini))
+
